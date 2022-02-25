88c9382351d611bb3588be4276a000671693165b
unknown <rajakongara2507@gmail.com>
2022-02-25 20:21:27 +0530

    [Raja]Add selection problem using switch case of Practice problems

--- PracticeProblem.js
+++ PracticeProblem.js
@@ -1,53 +1,87 @@
-//Sequence PracticeProble
+//Selection PracticeProble using the Switch Case
 //Read a single digit number and write the number in word
 let ran=Math.floor(Math.random()*10)+1;
-if(ran==1)
-console.log("One");
-else if(ran==2)
-console.log("Two");
-else if(ran==3)
-console.log("Three");
-else if(ran==4)
-console.log("Four");
-else if(ran==5)
-console.log("Five");
-else if(ran==6)
-console.log("Six")
-else if(ran==7)
-console.log("Seven")
-else if(ran==8)
-console.log("Eight")
-else
-console.log("Nine");
+switch(ran)
+{
+    case 1:
+      console.log("One");
+      break;
+    case 2:
+      console.log("Two");
+      break;
+    case 3:
+      console.log("Three");
+      break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+      console.log("Six");
+      break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    default:
+        console.log("Nine");
+        break;
+}
 //Read a Number and Display the week day
 let random=Math.floor(Math.random()*10)%7;
-if(random==0)
-console.log("SunDay");
-else if(random==1)
-console.log("MonDay");
-else if(random==2)
-console.log("TueDay");
-else if(random==3)
-console.log("WedDay");
-else if(random==4)
-console.log("ThurDay");
-else if(random==5)
-console.log("FroDay");
-else
-console.log("satDay");
+switch(random)
+{
+    case 0:
+        console.log("SunDay");
+        break;
+    case 1:
+        console.log("MonDay");
+        break;
+    case 2:
+        console.log("TueDay");
+        break;
+    case 3:
+        console.log("WedDay");
+        break;
+    case 4:
+        console.log("ThurDay");
+        break;
+    case 5:
+        console.log("Firday");
+        break;
+    default:
+        console.log("SatDay");
+        break;
+}
 //Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
 let num=9846;
-let thousand = (num /1000);
-let a=num%1000;
-console.log("Thousand : " + Math.floor(thousand));
-let hun = a / 100;
-console.log("Hundreds : " + Math.floor(hun));
-let b = num % 100;
-let ten = b/ 10 ;
-console.log("Tens : " + Math.floor(ten)) ;
-let c=num%10
-let one = c / 1
-console.log("Units : " + Math.floor(one));
+let rand=Math.floor(Math.random()*4)+1;
+switch(rand)
+{
+    case 1:
+        let thousand = (num /1000);
+        console.log("Thousand : " + Math.floor(thousand));
+        break;
+    case 2:
+        let a=num%1000;
+        let hun = a / 100;
+        console.log("Hundreds : " + Math.floor(hun));
+        break;
+    case 3:
+        let b = num % 100;
+        let ten = b/ 10 ;
+        console.log("Tens : " + Math.floor(ten)) ;
+        break;
+    case 4:
+        let c=num%10
+        let one = c / 1
+        console.log("Units : " + Math.floor(one));
+        break;
+}
 //Arthamatic operation
 let k=10;
 let l=20;
